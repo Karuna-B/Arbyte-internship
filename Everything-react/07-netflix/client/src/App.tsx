2ef15c9455d8eee71b59bf14e124ba44ec2deabd
Karuna Bhattarai <kb.karunabhattarai110@gmail.com>
2024-08-23 13:38:15 +0545

    static pages and routing

--- Everything-react/07-netflix/client/src/App.tsx
+++ Everything-react/07-netflix/client/src/App.tsx
@@ -1,9 +1,9 @@
-import HomePage from "./pages/HomePage";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Outlet />
     </div>
   );
 }
--- Everything-react/07-netflix/client/src/main.tsx
+++ Everything-react/07-netflix/client/src/main.tsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from "./App.tsx";
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import HomePage from "./pages/HomePage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+import PlansPage from "./pages/PlansPage.tsx";
+import BrowsePage from "./pages/BrowsePage.tsx";
+import WatchPage from "./pages/WatchPage.tsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/plans" element={<PlansPage />} />
+      <Route path="/browse" element={<BrowsePage />} />
+      <Route path="/browse/watch/:id" element={<WatchPage />} />
+    </Route>
+  )
+);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
