2d34809fbc7fa8c028883fa8b2a53c0af2a5c90c
Karuna Bhattarai <kb.karunabhattarai110@gmail.com>
2024-09-03 13:24:18 +0545

    starting out with node

---
+++ Everything-node/index.js
@@ -0,0 +1,6 @@
+// const { generateRandomNumber, celciusToFahrenheit } = require("./utils");
+import getPosts from "./postController.js";
+
+// console.log(`Random Number: ${generateRandomNumber()}`);
+// console.log(`In fahrenheit: ${celciusToFahrenheit(37)}`);
+console.log(getPosts());
