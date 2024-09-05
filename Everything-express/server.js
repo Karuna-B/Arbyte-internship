3ab9bb8c1d191481f57780bc12e4eb1b6fec315a
Karuna Bhattarai <kb.karunabhattarai110@gmail.com>
2024-09-05 14:56:30 +0545

    colors

--- Everything-express/server.js
+++ Everything-express/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import path from "path";
 import posts from "./routes/posts.js";
+import logger from "./middleware/logger.js";
+import errorHandler from "./middleware/error.js";
+import notFound from "./middleware/notFound.js";
 
 const port = process.env.PORT || 8080;
 
@@ -18,9 +21,19 @@ const app = express();
 // });
 
 //body parser middleware
-app.use(express.json( ));
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//logger middleware
+app.use(logger);
 //Routes
 app.use("/api/posts", posts);
+
+// setup static folder
+//app.use(express.static(path.join(__dirname, 'public')));
+
+// Error handler
+app.use(notFound);
+app.use(errorHandler);
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
